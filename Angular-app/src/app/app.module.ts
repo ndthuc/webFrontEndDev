39f8475205526a45d18bdee983ce025063136a90
unknown <thuc.nd@itsstraininng.edu.vn>
2020-05-27 21:13:58 +0700

    Giao dien

--- Angular-app/src/app/app.module.ts
+++ Angular-app/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular-app/src/environments/environment.ts
+++ Angular-app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD8sJRm1cJOURAUuPKU46BybQ0BvSM-XS4",
+    authDomain: "tin328dv01.firebaseapp.com",
+    databaseURL: "https://tin328dv01.firebaseio.com",
+    projectId: "tin328dv01",
+    storageBucket: "tin328dv01.appspot.com",
+    messagingSenderId: "661447561658",
+    appId: "1:661447561658:web:abbe8d35cac68b8ba9cd83"
+  }
 };
 
 /*
