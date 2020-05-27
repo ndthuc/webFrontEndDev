3bb0950fc6d9d7e321a5ad6e88094198e1416385
unknown <thuc.nd@itsstraininng.edu.vn>
2020-05-27 21:41:55 +0700

    routing + services[empls, prods, password]

--- Angular-app/src/app/app-routing.module.ts
+++ Angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,53 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ForgetPassComponent } from './components/forget-pass/forget-pass.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "settings",
+    component: SettingsComponent
+  },
+  {
+    path: "employees",
+    component: EmployeesComponent
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "help",
+    component: ForgetPassComponent
+  },
+  {
+    path: "",
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
